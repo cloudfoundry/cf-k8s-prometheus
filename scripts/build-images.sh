d58315c6708c4a59589a9916016bf5b9995e210d
Louie Brann <lbrann@vmware.com>
2020-08-26 22:58:58 +0000

    ubuntu-based node_exporter image
    [#173592534]

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,6 +1,6 @@
 DOCKER_ORG=${DOCKER_ORG:-oratos}
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
-DEPLAB=${DEPLAB:-true}
+DEPLAB=${DEPLAB:-false}
 
 function buildAndReplaceImage {
     image=$1
@@ -9,7 +9,7 @@ function buildAndReplaceImage {
     dockerfile=$4
 
     docker build --build-arg VERSION=$version -t $DOCKER_ORG/$image:$version $srcFolder -f $srcFolder/$dockerfile
-    if [ -n "$DEPLAB" ]; then
+    if [ "$DEPLAB" == "true" ]; then
         deplab --image $DOCKER_ORG/$image:$version --git ${REPO_DIR} --output-tar /tmp/$image.tgz --tag $DOCKER_ORG/$image:$version
         docker load -i /tmp/$image.tgz
     fi
@@ -17,3 +17,5 @@ function buildAndReplaceImage {
 }
 
 buildAndReplaceImage statsd_exporter v0.15.0 ${REPO_DIR}/exporters/statsd_exporter Dockerfile statsd_exporter
+buildAndReplaceImage node_exporter v0.18.1 ${REPO_DIR}/exporters/node_exporter Dockerfile
+
