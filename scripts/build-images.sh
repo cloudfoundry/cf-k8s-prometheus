37bad2a7630639ed2c98e23bbbfc15120cc876f7
Louie Brann <lbrann@vmware.com>
2020-10-20 20:59:38 +0000

    Switch to cloudfoundry dockerhub
    [#174735977]
    
    Signed-off-by: Caitlyn Yu <caitlyny@vmware.com>

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,4 +1,4 @@
-DOCKER_ORG=${DOCKER_ORG:-oratos}
+DOCKER_ORG=${DOCKER_ORG:-cloudfoundry}
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 DEPLAB=${DEPLAB:-false}
 
